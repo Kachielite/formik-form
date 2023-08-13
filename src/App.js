e65aad82c9a706ab0af34cdcdeea110fadb0fb07
Kachielite <40024356+Kachielite@users.noreply.github.com>
2023-08-13 09:28:58 +0100

    updated README and styling of forms

--- src/App.js
+++ src/App.js
@@ -2,63 +2,43 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center bg-slate-200 px-4">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full md:w-96 ">
-        <h1 className="text-center text-2xl my-6 font-bold">Sign up!!</h1>
-        <div className="mb-4 w-full">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="first_name"
-          >
-            First Name
-          </label>
+    <div className="container">
+      <form>
+        <h1 className="">Sign up!!</h1>
+        <div className="form_input_container">
+          <label htmlFor="first_name">First Name</label>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="form_input"
             id="first_name"
             type="text"
             placeholder="Enter your first name"
           />
         </div>
-        <div className="mb-4 w-full">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="first_name"
-          >
-            Last Name
-          </label>
+        <div className="form_input_container">
+          <label htmlFor="first_name">Last Name</label>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="form_input"
             id="last_name"
             type="text"
             placeholder="Enter your last name"
           />
         </div>
-        <div className="mb-4 w-full">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="first_name"
-          >
-            Email Address
-          </label>
+        <div className="form_input_container">
+          <label htmlFor="first_name">Email Address</label>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="form_input"
             id="email"
             type="text"
             placeholder="Enter your email"
           />
         </div>
-        <div className="flex items-center justify-between mt-10">
-          <button
-            className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-          >
+        <div className="form_btn_container">
+          <button className="btn" type="submit">
             Sign In
           </button>
         </div>
       </form>
-      <p className="text-center text-gray-500 text-xs">
-        &copy;2020 All rights reserved.
-      </p>
+      <p>&copy;2020 All rights reserved.</p>
     </div>
   );
 };
